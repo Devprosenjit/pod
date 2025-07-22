4b687a7b17e3316d2e6071c5e20cac27aa142d51
Devprosenjit <enprosenjit@gmail.com>
2025-07-22 13:01:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,200 +1,59 @@
-
-# Data block to reference existing Resource Group
-data "azurerm_resource_group" "existing_rg" {
-  name = "todorg"
+provider "azurerm" {
+  features {}
+  subscription_id = "ba3dd1c9-2ade-4648-a13f-65581e2a2874"
 }
 
-# Data block to reference existing Virtual Network
-data "azurerm_virtual_network" "existing_vnet" {
-  name                = "todo_vnet"
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-# Data block to reference existing Subnet
-data "azurerm_subnet" "existing_subnet" {
-  name                 = "todo_subnet"
-  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
-  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-
-# Create Public IP
-resource "azurerm_public_ip" "linux_vm_ip" {
-  name                = "linuxVM-ip"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  allocation_method   = "Static"
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create NIC for VM
-resource "azurerm_network_interface" "linux_vm_nic" {
-  name                = "linuxVM-nic"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.existing_subnet.id
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.linux_vm_ip.id
   }
 }
 
-#Create Linux VM
-resource "azurerm_linux_virtual_machine" "linux_vm" {
-  name                  = "forntendvm"
-  location              = data.azurerm_resource_group.existing_rg.location
-  resource_group_name   = data.azurerm_resource_group.existing_rg.name
-  size                  = "Standard_B1s"
-  admin_username        = "devops"
-  admin_password        = "devops@12345"  # Use ssh key in production
-  network_interface_ids = [azurerm_network_interface.linux_vm_nic.id]
-
-  disable_password_authentication = false
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
   os_disk {
-    name                 = "linuxOSDisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
-  custom_data = base64encode(<<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-              apt-get update
-              apt-get install -y docker-ce
-              systemctl enable docker
-              systemctl start docker
-
-              # Pull and run docker container (replace nginx with your own image if needed)
-              docker run -d -p 80:80 nginx
-            EOF
-  )
-
-
-
 }
-
-
-# # more rg add
-resource "azurerm_resource_group" "rg" {
-  name = "debu"
-  location ="centralindia"
-  
-}
-
-
-
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_web_application_firewall_policy" "example" {
-  name                = "example-wafpolicy"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  custom_rules {
-    name      = "Rule1"
-    priority  = 1
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
-    }
-
-    action = "Block"
-  }
-
-  custom_rules {
-    name      = "Rule2"
-    priority  = 2
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values       = ["192.168.1.0/24"]
-    }
-
-    match_conditions {
-      match_variables {
-        variable_name = "RequestHeaders"
-        selector      = "UserAgent"
-      }
-
-      operator           = "Contains"
-      negation_condition = false
-      match_values       = ["Windows"]
-    }
-
-    action = "Block"
-  }
-
-  policy_settings {
-    enabled                     = true
-    mode                        = "Prevention"
-    request_body_check          = true
-    file_upload_limit_in_mb     = 100
-    max_request_body_size_in_kb = 128
-  }
-
-  managed_rules {
-    exclusion {
-      match_variable          = "RequestHeaderNames"
-      selector                = "x-company-secret-header"
-      selector_match_operator = "Equals"
-    }
-    exclusion {
-      match_variable          = "RequestCookieNames"
-      selector                = "too-tasty"
-      selector_match_operator = "EndsWith"
-    }
-
-    managed_rule_set {
-      type    = "OWASP"
-      version = "3.2"
-      rule_group_override {
-        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
-        rule {
-          id      = "920300"
-          enabled = true
-          action  = "Log"
-        }
-
-        rule {
-          id      = "920440"
-          enabled = true
-          action  = "Block"
-        }
-      }
-    }
-  }
-
-}
-
-check our code
-
-check your code
-
